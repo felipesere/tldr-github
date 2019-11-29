c8f2863c992de87b1bdd84908496791aaaf7ef67
Felipe Sere <felipesere@gmail.com>
2019-11-29 16:54:15 +0100

    Extract the static_files router

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ use std::fs::File;
 
 use async_std::task;
 
-use tide::{ Middleware, Next, Request, Response};
 use futures::future::BoxFuture;
+
+use tide::{Middleware, Next, Request, Response};
+
+mod static_files;
 
 use simplelog::*;
 
@@ -36,6 +39,7 @@ impl RequestLogger {
         );
         res
     }
+
 }
 
 impl<State: Send + Sync + 'static> Middleware<State> for RequestLogger {
@@ -52,31 +56,14 @@ fn main() -> Result<(), std::io::Error> {
         ]
     ).unwrap();
 
+
+    let files = crate::static_files::new::<()>();
+
     let mut app = tide::new();
     app.middleware(RequestLogger{});
     app.at("/").get(tide::redirect("/index.html"));
+    app.at("/files").nest(files.router());
 
-    // TODO:    turn this into a "static files" router that matches against local files
-    //          exclude evil paths like "/.."
-    //          https://github.com/SergioBenitez/Rocket/blob/da7e022f990e0b8e8201b0a359a43104686ff1a4/core/http/src/uri/segments.rs#L65
-    app.at("/*filename").get(|req: Request<()>| async move {
-        let filename: String = req.param("filename").unwrap();
-        log::warn!("The filename was: {}", filename);
-
-        match async_std::fs::read_to_string(&filename).await {
-            Ok(content) => {
-                let path = std::path::Path::new(&filename);
-                match path.extension() {
-                    Some(extension) => { 
-                        let content_type = format!("text/{}", (*extension).to_str().unwrap());
-                        Response::new(200).body_string(content).set_header("Content-Type", content_type)
-                    },
-                    None => Response::new(200).body_string(content),
-                }
-            },
-            Err(_) => Response::new(404),
-        }
-    });
     task::block_on(async move {
         app.listen("127.0.0.1:8080").await;
     });
---
+++ src/static_files.rs
@@ -0,0 +1,39 @@
+use tide::{ Request, Response};
+
+// TODO:turn this into a "static files" router that matches against local files
+//      exclude evil paths like "/.."
+//      https://github.com/SergioBenitez/Rocket/blob/da7e022f990e0b8e8201b0a359a43104686ff1a4/core/http/src/uri/segments.rs#L65
+pub struct StaticFiles<STATE> {
+    _marker: std::marker::PhantomData<STATE>,
+}
+
+pub fn new<STATE>() -> StaticFiles<STATE> {
+    return StaticFiles {
+        _marker: std::marker::PhantomData,
+    }
+}
+
+impl <STATE: Send + Sync + Copy + 'static> StaticFiles<STATE> {
+    pub fn router(self) -> impl FnOnce(&mut tide::Route<'_, STATE>) {
+        return |base: &mut tide::Route<STATE>| {
+            base.at("/*filename").get(|req: Request<STATE>| async move {
+                let filename: String = req.param("filename").unwrap();
+                log::warn!("The filename was: {}", filename);
+
+                match async_std::fs::read_to_string(&filename).await {
+                    Ok(content) => {
+                        let path = std::path::Path::new(&filename);
+                        match path.extension() {
+                            Some(extension) => { 
+                                let content_type = format!("text/{}", (*extension).to_str().unwrap());
+                                Response::new(200).body_string(content).set_header("Content-Type", content_type)
+                            },
+                            None => Response::new(200).body_string(content),
+                        }
+                    },
+                    Err(_) => Response::new(404),
+                }
+            });
+        }
+    }
+}
