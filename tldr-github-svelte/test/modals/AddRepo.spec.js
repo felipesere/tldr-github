79482a127dfe1e0664e1d474e8b0387f47528d4f
Felipe Sere <felipesere@gmail.com>
2020-01-28 20:53:23 +0100

    Sample for bubbling up an event

---
+++ tldr-github-svelte/test/modals/AddRepo.spec.js
@@ -0,0 +1,24 @@
+import {render, fireEvent, cleanup} from "@testing-library/svelte";
+import AddRepo from '../../src/AddRepo.svelte'
+import {enableMocks} from 'jest-fetch-mock'
+
+enableMocks();
+
+describe("add repo", () => {
+    beforeEach(() => {
+        fetchMock.resetMocks();
+        cleanup()
+    });
+
+    it("bubbles up an event", async () => {
+        fetchMock.mockReject("it failed");
+        const {findByPlaceholderText, findByText, component} = render(AddRepo);
+        component.$on('new-repo-added', (e) => console.log(e.detail));
+
+        let input = await findByPlaceholderText("Add new repo");
+        let button = await findByText("Add");
+
+        await fireEvent.input(input, {target: {value: "foo/bar"}});
+        await fireEvent.click(button);
+    });
+});
