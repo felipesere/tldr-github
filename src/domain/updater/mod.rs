62b29cf3e6aa0fac5677a3b7431aecd0c0785271
Felipe Sere <felipesere@gmail.com>
2020-03-12 20:01:25 +0100

    Remove pointless title

--- src/domain/updater/mod.rs
+++ src/domain/updater/mod.rs
@@ -31,7 +31,6 @@ pub fn start(config: Config) {
 
             let updated = updated.unwrap();
 
-            let title = item.title.clone();
             let result = match update(item, updated) {
                 Outcome::Update(u) => db.update_tracked_item(&repo, u),
                 Outcome::Remove(u) => db.remove_tracked_item(&repo, u),
